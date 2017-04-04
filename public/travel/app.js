2a04d8cace93a4635c26d62d03db06fc0ddea79b
yuriy <yuriy.turetskiy@gmail.com>
2017-04-03 22:12:35 -0400

    fixed google maps thing

--- public/travel/app.js
+++ public/travel/app.js
@@ -3,8 +3,20 @@ console.log("it's on")
 
 var newLocation = ""
 
+      function initMap() {
+        var uluru = {lat: newLocation.latitude, lng: newLocation.longitude};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 6,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
 
 window.onload = function () {
+
 		$.ajax({
 		  url: "https://travel-api-app.herokuapp.com/random",
 		  success: function(data){newLocation = data},
@@ -14,6 +26,11 @@ window.onload = function () {
 		$(".location").text(newLocation.name);
 		$(".why-text").text(newLocation.description);
 		$($(".photo")[0]).attr("src",newLocation.img_url);
+		initMap();
 	});
 	
+
+
+
+    
 }
