b00200b7eacefad09c094340b6e6f652a6a71f85
yuriy <yuriy.turetskiy@gmail.com>
2017-04-22 15:59:17 -0400

    all working well

--- public/travel/app.js
+++ public/travel/app.js
@@ -1,9 +1,11 @@
 
 console.log("it's on")
 
+var allLocations = []
+var locationCount = 0
 var newLocation = ""
 
-      function initMap() {
+      var initMap = function() {
         var uluru = {lat: newLocation.latitude, lng: newLocation.longitude};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 6,
@@ -16,26 +18,33 @@ var newLocation = ""
       }
 
 $(".refresh").click(function(){
-	location.reload();
+	$($(".photo")[0]).attr("src","");
+	locationCount +1 >= allLocations.length ? locationCount = 0 : locationCount +=1
+	newLocation = allLocations[locationCount]
+	replaceContent()
 })
 
-window.onload = function () {
+var replaceContent = function () {
+	$($(".photo")[0]).attr("src",newLocation.img_url);
+	setTimeout(function(){
+		$(".location").text(newLocation.name);
+		$(".why-text").text(newLocation.description);
+		initMap();
+	},100);
 
-var getLocation = function(){		
+}
+var getLocations = function(){		
 	$.ajax({
 		  url: "https://travel-api-app.herokuapp.com/random",
-		  success: function(data){newLocation = data},
+		  success: function(data){allLocations = data},
 		  crossDomain:true,
 		  dataType: "json"
 	}).done(function(){
-		$(".location").text(newLocation.name);
-		$(".why-text").text(newLocation.description);
-		$($(".photo")[0]).attr("src",newLocation.img_url);
-		initMap();
+		newLocation = allLocations[0];
+		replaceContent();
 	});
 }
-getLocation()
 
-
-    
+window.onload = function () {
+	getLocations();
 }
